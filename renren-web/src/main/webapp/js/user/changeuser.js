7829c147fe8e28d619f74606444e1574f2ac6bc9
momozqy <532676071@qq.com>
2017-07-21 16:19:40 +0800

    增加更过用户类型菜单

---
+++ renren-web/src/main/webapp/js/user/changeuser.js
@@ -0,0 +1,84 @@
+$(function () {
+
+});
+
+var vm = new Vue({
+	el:'#rrapp',
+	data:{
+		showList: true,
+		title: null,
+		ctUser: {}
+	},
+	methods: {
+		query: function () {
+			vm.reload();
+		},
+		add: function(){
+			vm.showList = false;
+			vm.title = "新增";
+			vm.ctUser = {};
+		},
+		update: function (event) {
+			var userId = getSelectedRow();
+			if(userId == null){
+				return ;
+			}
+			vm.showList = false;
+            vm.title = "修改";
+            
+            vm.getInfo(userId)
+		},
+		saveOrUpdate: function (event) {
+			var url = vm.ctUser.userId == null ? "../ctuser/save" : "../ctuser/update";
+			$.ajax({
+				type: "POST",
+			    url: url,
+			    data: JSON.stringify(vm.ctUser),
+			    success: function(r){
+			    	if(r.code === 0){
+						alert('操作成功', function(index){
+							vm.reload();
+						});
+					}else{
+						alert(r.msg);
+					}
+				}
+			});
+		},
+		del: function (event) {
+			var userIds = getSelectedRows();
+			if(userIds == null){
+				return ;
+			}
+			
+			confirm('确定要删除选中的记录？', function(){
+				$.ajax({
+					type: "POST",
+				    url: "../ctuser/delete",
+				    data: JSON.stringify(userIds),
+				    success: function(r){
+						if(r.code == 0){
+							alert('操作成功', function(index){
+								$("#jqGrid").trigger("reloadGrid");
+							});
+						}else{
+							alert(r.msg);
+						}
+					}
+				});
+			});
+		},
+		getInfo: function(userId){
+			$.get("../ctuser/info/"+userId, function(r){
+                vm.ctUser = r.ctUser;
+            });
+		},
+		reload: function (event) {
+			vm.showList = true;
+			var page = $("#jqGrid").jqGrid('getGridParam','page');
+			$("#jqGrid").jqGrid('setGridParam',{ 
+                page:page
+            }).trigger("reloadGrid");
+		}
+	}
+});
